861719ece2a26c31e04bdc1730b13c576c69e638
Ilya <ilyaiparshin@gmail.com>
2017-10-06 17:15:43 +0500

    1. Дополнено описание 2. Деструктор объекта

---
+++ conf.py
@@ -0,0 +1,11 @@
+params = {
+    "Port": "TCPIP",
+    "IPAddress": "192.168.x.x",
+    "IPPort": "5555",
+    "Model": "62",
+    "AccessPassword": "0",
+    "UserPassword": "30",
+    "Protocol": "2",
+    "LogFileName": "/var/www/kkt/kkt.log",
+    "TestMode": False
+}
